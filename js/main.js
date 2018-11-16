8ea4ece6c4c622dea45c973bf4c0ed50a88b11df
Golberg Mark <golbergmark@gmail.com>
2018-11-17 00:49:31 +0300

    End.

--- js/main.js
+++ js/main.js
@@ -2,27 +2,36 @@
 {
 	'use strict';
 
+	var menuBtn = $('.menu__btn'),
+		closeMenuBtn = $('.menu-close__btn'),
+		navList = $('.nav-list'),
+		layout = $('.layout');
+
 	$('.menu__link').click(function() 
 	{
 		var $page = $('html, body');
 	    $page.animate({
 	        scrollTop: $($.attr(this, 'href')).offset().top
 	    }, 800);
+
+	    layout.css('display', 'none');
+
    		return false;
 	});
-
-	var menuBtn = $('.menu__btn'),
-		closeMenuBtn = $('.menu-close__btn'),
-		navList = $('.nav-list');
 
 	menuBtn.click(function(evt)
 	{
 		evt.preventDefault();
+
+		var htmlWidth = $('body').css('width'),
+			htmlHeight = $('body').css('height');
 		
 		navList.slideDown(350);
 
 		menuBtn.toggleClass('visually-hidden');
 		closeMenuBtn.toggleClass('visually-hidden');
+
+		layout.css('display', 'block').css('width', htmlWidth).css('height', htmlHeight);
 	});
 
 	closeMenuBtn.click(function(evt)
@@ -33,6 +42,17 @@
 		
 		menuBtn.toggleClass('visually-hidden');
 		closeMenuBtn.toggleClass('visually-hidden');
+		layout.css('display', 'none');
+	});
 
+	layout.click(function(evt)
+	{
+		evt.preventDefault();
+
+		navList.slideUp(350);
+		
+		menuBtn.toggleClass('visually-hidden');
+		closeMenuBtn.toggleClass('visually-hidden');
+		layout.css('display', 'none');
 	});
 })($);
